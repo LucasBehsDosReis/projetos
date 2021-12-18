a2cef1b93ff6ed6249cbb2935a639f088b65f421
lucasbehsdosreis <lucasbehsdosreis.projetos@gmail.com>
2021-12-18 17:12:08 -0300

    exemplos da aula 1

---
+++ type-primitive/type-string.js
@@ -0,0 +1,20 @@
+const texto1    = "Olá, mundo!";
+const texto2    = 'Olá, mundo!';
+const senha     = "senhaSuperSegura456!";
+const stringDeNumeros   = "123456";
+
+// Pode se usar tanto "" quanto '', porém em citações é preciso usar os 2
+
+const citacao1  = 'O lucas disse "olá!"';
+const citacao2  = "O lucas disse 'olá!'";
+
+// concatenação de strings (+)
+
+const frase = "Meu nome é "
+const nome  = "Lucas"
+
+const fraseNome = frase + nome;
+
+console.log(fraseNome)
+
+// template string OU template literal
